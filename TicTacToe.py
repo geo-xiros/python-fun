a831233206126b5a10f9cd33ff316d8a89061d1f
george <geo.xiros@gmail.com>
2018-10-18 16:04:34 +0300

    Update TicTacToe.py
    add player 1 vs computer

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,5 @@
+import random
+
 board = ['1','2','3',
          '4','5','6',
          '7','8','9']
@@ -11,7 +13,8 @@ winTests = (((2,3),(4,7),(5,9)),        #1
             ((1,4),(8,9),(5,3)),        #7
             ((7,9),(2,5)),              #8
             ((7,8),(3,6),(1,5)))        #9
-PlayersChoices = []
+
+PlayersChoices = ['1','2','3','4','5','6','7','8','9']
 
 def player_wins(player, pos):
     
@@ -27,27 +30,47 @@ def player_wins(player, pos):
         
 def print_board():
     for i in range(0,9,3):
-        print('   |   |   ')
-        print(' ' + board[i] + ' | ' + board[i+1] + ' | ' + board[i+2] + ' ')
-        print('   |   |   ')
+        print('      |      |      ')
+        print('  ' + board[i] + board[i] + '  |  ' + board[i+1] + board[i+1] + '  |  ' + board[i+2] + board[i+2] + '  ')
+        print('  ' + board[i] + board[i] + '  |  ' + board[i+1] + board[i+1] + '  |  ' + board[i+2] + board[i+2] + '  ')
+        print('      |      |      ')
         if i<=3:
-            print('---|---|---')
+            print('------|------|------')
 
 def player_choice(player):
     inp = input(player + ' select from 1 to 9: ')
-    while len(inp)!=1 or not inp.isnumeric() or inp in PlayersChoices:
+    while len(inp)!=1 or not inp.isnumeric() or not inp in PlayersChoices:
         inp = input(player + ' select from 1 to 9: ')
-    PlayersChoices.append(inp)
+    PlayersChoices.remove(inp)
     return int(inp)-1
-    
-def play_game(players, playersLetter):
 
+def computer_choice(player):
+    rand = random.choice(PlayersChoices)
+    PlayersChoices.remove(rand)
+    print (player + ' selection is ' + rand)
+    return int(rand)-1
+
+def last_move(playerLetter):
+    for i in PlayersChoices:
+        if player_wins(playerLetter,int(i)-1):
+            return int(i)-1
+    return 0
+
+def next_choice(playerLetter, player):
+    pc = last_move(playerLetter)
+    if pc != 0:
+        return pc
+    return player_choice(player)
+    
+def play_game(players, playersLetter, choiceFunc):
+    
     player = 0
     print_board()
     
-    for i in range(9):
+    while len(PlayersChoices) != 0:
+
+        playerChoice = choiceFunc[player](players[player])
         
-        playerChoice = player_choice(players[player])
         board[playerChoice] = playersLetter[player]
         print_board()
         
@@ -60,5 +83,7 @@ def play_game(players, playersLetter):
             player = 0
     return 'No one'
 
-print(play_game(['Player x','Player o'], ['x','o']) + ' Wins !!!')
+#print(play_game(['Player x','Player o'], ['x','o'],[player_choice, player_choice]) + ' Wins !!!')
+print(play_game(['Player x','Computer o'], ['x','o'],[player_choice, computer_choice]) + ' Wins !!!')
+
 
